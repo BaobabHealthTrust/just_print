cef1e986d24a600f990a8301f1a4bbf110065e3e
jmziya <jacobmziya@gmail.com>
2018-12-04 10:55:07 +0200

    Made the install script to automatically get the system username and the default profile for mozilla firefox running on the installation machine.

--- install.sh
+++ install.sh
@@ -2,19 +2,43 @@
 #
 # Installation script
 
+#====================================================================================================================#
+#                                                                                                                    #
+# MAKE SURE THIS SCRIPT IS NOT RUN AS ROOT                                                                           #
+#                                                                                                                    #
+# RUN THIS SCRIPT AS THE USER THAT WILL BE USED TO RUN THE APPLICATION IN BROWSER                                    #
+#                                                                                                                    #
+#====================================================================================================================#
+
 # browser integration
-cp resources/mimeTypes.rdf /home/user/.mozilla/firefox/default.profile/
+# auto-read system username and default mozilla firefox profile
+
+# get just print directory before excuting anything else
+JUST_PRINT_DIRECTORY=$(pwd)
+
+cd ~/.mozilla/firefox/
+
+if [[ $(grep '\[Profile[^0]\]' profiles.ini) ]]
+then PROFPATH=$(grep -E '^\[Profile|^Path|^Default' profiles.ini | grep -1 '^Default=1' | grep '^Path' | cut -c6-)
+else PROFPATH=$(grep 'Path=' profiles.ini | sed 's/^Path=//')
+fi
+
+echo $PROFPATH
+
+cd $JUST_PRINT_DIRECTORY
+
+sudo cp resources/mimeTypes.rdf /home/$USER/.mozilla/firefox/$PROFPATH/
 
 # print-server
-cp bin/netcat_server.sh /usr/local/sbin/
+sudo cp bin/netcat_server.sh /usr/local/sbin/
 
 # print-proxy
-cp bin/print /usr/lib/cgi-bin/
+sudo cp bin/print /usr/lib/cgi-bin/
 
 # print-client
-cp bin/print.rb /home/user/
-cp bin/print_via_cgi.rb /home/user/
+sudo cp bin/print.rb /home/$USER/
+sudo cp bin/print_via_cgi.rb /home/$USER/
 
 # startup script
-cp resources/rc.local /etc/
+sudo cp resources/rc.local /etc/
 
